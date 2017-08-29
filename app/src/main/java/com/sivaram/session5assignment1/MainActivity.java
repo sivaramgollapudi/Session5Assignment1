1a841cd378f948f81b78d7eee7e344e7361f3675
sivaramgollapudi <sivaramgollapudi@hotmail.com>
2017-08-29 18:16:19 +0800

    Added Button to Start And Stop Animation.

--- app/src/main/java/com/sivaram/session5assignment1/MainActivity.java
+++ app/src/main/java/com/sivaram/session5assignment1/MainActivity.java
@@ -6,28 +6,51 @@ import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Animation animation;
+    TextView lblBlink;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        PerformBlinkingAnimation();
+
+        // Initialize Button Click Listener
+
+        final Button btnAnimate = (Button)findViewById(R.id.btnStartAnimation);
+
+        btnAnimate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btnAnimate.getText().toString().equalsIgnoreCase(getResources().getString(R.string.startanimation))) {
+                    PerformBlinkingAnimation(btnAnimate.getText().toString());
+                    btnAnimate.setText(R.string.stopanimation);
+                }
+                else {
+                    PerformBlinkingAnimation(btnAnimate.getText().toString());
+                    btnAnimate.setText(R.string.startanimation);
+                }
+            }
+        });
 
     }
 
-    private void PerformBlinkingAnimation(){
+    private void PerformBlinkingAnimation(String type){
         // Find TextView And Perform Animation
-        TextView lblBlink = (TextView)findViewById(R.id.lblFadeInAnimation);
-        animation = new AlphaAnimation(0.0f,1.0f);
-        animation.setDuration(500);
-        animation.setStartOffset(100);
-        animation.setRepeatMode(Animation.REVERSE);
-        animation.setRepeatCount(Animation.INFINITE);
-        lblBlink.startAnimation(animation);
-
+        lblBlink = (TextView)findViewById(R.id.lblFadeInAnimation);
+        if (type.equalsIgnoreCase("Start Animation")) {
+            animation = new AlphaAnimation(0.0f, 1.0f);
+            animation.setDuration(500);
+            animation.setStartOffset(100);
+            animation.setRepeatMode(Animation.REVERSE);
+            animation.setRepeatCount(Animation.INFINITE);
+            lblBlink.startAnimation(animation);
+        }
+        else
+            lblBlink.clearAnimation();
     }
 }
